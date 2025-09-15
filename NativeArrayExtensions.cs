b5f2385e9d257963f97ca4b6d4af6c4791c06190
Logan <qingwabote@126.com>
2025-09-15 08:59:14 +0800

    NativeArrayExtensions: ref readonly ElementAt of NativeArray.ReadOnly

--- NativeArrayExtensions.cs
+++ NativeArrayExtensions.cs
@@ -9,6 +9,27 @@ namespace Budget
     public static unsafe class NativeArrayExtensions
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ref readonly T ElementAt<T>(this NativeArray<T>.ReadOnly array, int index) where T : unmanaged
+        {
+            CheckElementReadAccess(array, index);
+            return ref UnsafeUtility.ArrayElementAsRef<T>(array.GetUnsafeReadOnlyPtr(), index);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [Conditional("ENABLE_UNITY_COLLECTIONS_CHECKS")]
+        private static void CheckElementReadAccess<T>(NativeArray<T>.ReadOnly array, int index) where T : unmanaged
+        {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+            // if (index < 0 || index > array.Length)
+            // {
+            //     FailOutOfRangeError(index, array.Length);
+            // }
+            // AtomicSafetyHandle.CheckReadAndThrow(NativeArrayUnsafeUtility.GetAtomicSafetyHandle(array));
+            var _ = array[index];
+#endif
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ref T ElementAt<T>(this NativeArray<T> array, int index) where T : unmanaged
         {
             CheckElementWriteAccess(array, index);
